bde9f2f30bc45071b71d6ed088f8f52c26d9b7e7
Jason Chang <jasonchang0@berkeley.edu>
2018-10-28 07:31:55 -0700

    data_structures subfolder created

---
+++ data_structures/LinkedList.py
@@ -0,0 +1,172 @@
+class LinkedList(object):
+
+    def __init__(self, head=None, tail=None, fromArray=None):
+        if head and fromArray:
+            raise Exception('Bad Argument!')
+
+        if fromArray is not None:
+            self._fromArray(fromArray)
+            return
+
+        self.head = head
+        self.tail = tail
+        self.size = 0
+
+    class Node(object):
+
+        def __init__(self, val=None, next=None):
+            self.val = val
+            self.next = next
+
+        def isEmpty(self):
+            return not self.next
+        
+    def add(self, index, element):
+        i_site = self.head
+
+        if index > self.size:
+            raise Exception('Bad Index.')
+
+        if index == self.size:
+            self.tail = self.Node(e, self.tail)
+            return
+
+        for i in range(index):
+            i_site = i_site.next
+
+        i_site.next = i_site
+        i_site.val = element
+        self.size += 1
+
+    def addFirst(self, e):
+        i_site = self.Node(e, self.head)
+        self.head = i_site
+        self.size += 1
+
+    def addLast(self, e):
+        i_site = self.head
+
+        if not i_site:
+            self.head = self.Node(val=e)
+            self.tail = self.head
+            self.size += 1
+            return
+
+        while not i_site.isEmpty():
+            i_site = i_site.next
+
+        i_site.next = self.Node(val=e)
+        self.tail = i_site.next
+        self.size += 1
+
+    def clear(self):
+        self.__init__()
+
+    def clone(self):
+        return self.head
+
+    def isEmpty(self):
+        return not self.size and not self.head
+
+    def contain(self, e):
+        current = self.head
+
+        while not current:
+            if current.val == e or current.val is e:
+                return True
+
+            current = current.next
+
+        return False
+
+    def getFirst(self):
+        return self.head.val
+
+    def getLast(self):
+        return self.tail.val
+
+    def indexOf(self, e):
+        current = self.head
+        index = 0
+
+        while not current:
+            if current.val == e or current.val is e:
+                return index
+
+            current = current.next
+            index += 1
+
+        return -1
+
+    def remove(self, index):
+        r_site = self.head
+
+        if index > self.size:
+            raise Exception('Bad Index.')
+
+        for i in range(index - 1):
+            r_site = r_site.next
+
+        r_site.next = None
+        self.size -= 1
+
+    def removeFirst(self):
+        if not self.isEmpty():
+            self.head = self.head.next
+            self.size -= 1
+
+    def removeLast(self):
+        current = self.head
+
+        if not current or current.isEmpty():
+            self.__init__()
+            return
+
+        while current.next.next:
+            current = current.next
+
+        current.next = None
+        self.size -= 1
+
+    def set(self, index, element):
+        s_site = self.head
+
+        if index > self.size:
+            raise Exception('Bad Index.')
+
+        if index == self.size:
+            self.tail = self.Node(val=e)
+            return
+
+        for i in range(index):
+            s_site = s_site.next
+            s_site.val = element
+
+    def _fromArray(self, arr):
+        self.head = self.Node(val=arr[0])
+        self.tail = self.head.next
+        self.size = 0
+
+        for i in arr[1:]:
+            self.addLast(i)
+
+    def toArray(self):
+        out_array = []
+        current = self.head
+
+        if not current:
+            return out_array
+
+        while not current.isEmpty():
+            out_array += [current.val]
+            current = current.next
+            # print(out_array)
+
+        out_array.append(current.val)
+
+        return out_array
+
+
+
+
+
---
+++ data_structures/TestLinkedList.py
@@ -0,0 +1,101 @@
+import unittest
+import LinkedList as ll
+import numpy as np
+import sys
+
+
+class TestLinkedList(unittest.TestCase):
+    def testArrayEquals(self):
+        lst = [1, 2, 3, 4, 5]
+        linked_lst = ll.LinkedList(fromArray=lst)
+
+        # self.assertEquals(expected, observed)
+        self.assertEqual(lst, linked_lst.toArray())
+
+    def testAddFirst(self):
+        lst = [1, 3, 5, 7, 9]
+        linked_lst = ll.LinkedList()
+        linked_lst.addFirst(lst[0])
+        lst = lst[1:]
+
+        print(linked_lst.toArray())
+
+        for i in range(len(lst)):
+            linked_lst.addFirst(lst[i])
+
+        self.assertEqual([1, 3, 5, 7, 9], linked_lst.toArray())
+
+    def testAddLast(self):
+        lst = [1, 3, 5, 7, 9]
+        linked_lst = ll.LinkedList()
+        linked_lst.addLast(lst[-1])
+        lst = lst[:-1]
+
+        print(str(linked_lst.toArray()))
+
+        for i in range(len(lst)-1, -1, -1):
+            linked_lst.addLast(lst[i])
+
+        self.assertEqual(list(reversed([1, 3, 5, 7, 9])), linked_lst.toArray())
+
+    def testRemoveFirst(self):
+        lst = [1, 3, 5, 7, 9]
+        linked_lst = ll.LinkedList(fromArray=lst)
+        linked_lst.removeFirst()
+        lst = lst[1:]
+
+        print(linked_lst.toArray())
+
+        for i in range(len(lst)):
+            linked_lst.removeFirst()
+
+        self.assertEqual([], linked_lst.toArray())
+
+    def testRemoveLast(self):
+        lst = [1, 3, 5, 7, 9]
+        linked_lst = ll.LinkedList(fromArray=lst)
+        linked_lst.removeLast()
+        lst = lst[:-1]
+
+        print(linked_lst.toArray())
+
+        for i in range(len(lst)):
+            linked_lst.removeLast()
+
+        self.assertEqual([], linked_lst.toArray())
+
+    def testRandomArrayEquals(self):
+        lst = np.random.randint(sys.maxsize, size=10)
+        linked_lst = ll.LinkedList(fromArray=lst)
+        ops = ['addFirst', 'addLast', 'removeFirst', 'removeLast']
+
+        for i in range(50):
+            op = ops[np.random.randint(len(ops))]
+            e = np.random.randint(sys.maxsize)
+
+            if op == 'addFirst':
+                lst = [e] + lst
+            elif op == 'addLast':
+                lst += [e]
+            elif op == 'removeFirst':
+                lst = lst[1:]
+            else:
+                lst = lst[:-1]
+
+            if 'add' not in op:
+                exec('linked_lst.' + op + '()')
+                continue
+
+            exec('linked_lst.' + op + '(' + str(e) + ')')
+
+        print(str(lst))
+        print(str(linked_lst.toArray()))
+        self.assertEqual(lst, linked_lst.toArray())
+
+# Run the Tests
+    if __name__ == '__main__':
+        unittest.main()
+
+
+
+
