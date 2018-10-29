40d502c60f1e32c2c4beef98dfa5488608185d07
Jason Chang <jasonchang0@berkeley.edu>
2018-10-28 18:08:50 -0700

    hash tables/maps

---
+++ data_structures/HashMaps.py
@@ -0,0 +1,10 @@
+# Dictionaries, Maps, and Hash Tables in Python -> Use built-in dict instead
+
+# instantiate a dictionary from keylist and valuelist
+d = dict(zip(keys, values))
+
+# List of tuples
+listofTuples = [("Hello", 7), ("hi", 10), ("there", 45), ("at", 23), ("this", 77)]
+
+# Create and Initialize a dict by tuples
+lexicon = dict(listofTuples)
